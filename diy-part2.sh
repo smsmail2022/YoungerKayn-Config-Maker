3306a1b718977b1ce56de7bbae73c701d4a2903d
smsmail2022 <123883726+smsmail2022@users.noreply.github.com>
2023-10-04 09:27:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # This is an example, you can edit/remove it as needed.
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
